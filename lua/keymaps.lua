b82568cdb29eac484a4f25d1c49f6902f9908026
John Doe <johndoe@example.com>
2022-10-15 17:14:43 -0500

    Help me god I've broken the lsp server

---
+++ lua/keymaps.lua
@@ -0,0 +1,27 @@
+-- Aliases
+local map = vim.api.nvim_set_keymap
+local default_opts = {noremap = true, silent = true}
+local cmd = vim.cmd
+
+map('', '<up>', '<nop>', {noremap = true})
+map('', '<down>', '<nop>', {noremap = true})
+map('', '<left>', '<nop>', {noremap = true})
+map('', '<right>', '<nop>', {noremap = true})
+
+map('i', '<C-h>', '<left>', default_opts)
+map('i', '<C-j>', '<down>', default_opts)
+map('i', '<C-k>', '<up>', default_opts)
+map('i', '<C-l>', '<right>', default_opts)
+
+-- Vimspector
+cmd([[
+nmap <F9> <cmd>call vimspector#Launch()<cr>
+nmap <F5> <cmd>call vimspector#StepOver()<cr>
+nmap <F8> <cmd>call vimspector#Reset()<cr>
+nmap <F11> <cmd>call vimspector#StepOver()<cr>")
+nmap <F12> <cmd>call vimspector#StepOut()<cr>")
+nmap <F10> <cmd>call vimspector#StepInto()<cr>")
+]])
+map('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>", {noremap = true})
+map('n', "Dw", ":call vimspector#AddWatch()<cr>", {noremap = true})
+map('n', "De", ":call vimspector#Evaluate()<cr>", {noremap = true})
---
+++ lua/settings.lua
@@ -0,0 +1,14 @@
+-- Neovim API aliases
+local cmd = vim.cmd
+local exec = vim.api.nvim_exec
+local fn = vim.fn
+local g = vim.g
+local opt = vim.opt
+-- General configuration
+g.mapleader = ";"
+-- UI
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false
