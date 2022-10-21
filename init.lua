34cd3d0cafa143c3eacf6a868cf5c02bda836529
John Doe <johndoe@example.com>
2022-10-20 21:29:11 -0500

    Added Github dark color theme as default and basic configuration for it, commented Ayu requirement in init.lua file

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require('settings')
 require('keymaps')
 -- Plugins
 require('plugins/packer')		-- plugin manager
-require('plugins/ayu')
+--require('plugins/ayu')
 require("mason").setup()
 require("mason-lspconfig").setup()
 local rt = {
@@ -126,4 +126,24 @@ cmp.setup({
           return item
       end,
   },
+})
+-- Lua
+require("github-theme").setup({
+	theme_style = "dark_default",
+	-- other config
+	function_style = "italic",
+  	sidebars = {"qf", "vista_kind", "terminal", "packer"},
+
+  	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  	colors = {hint = "orange", error = "#ff0000"},
+
+  	-- Overwrite the highlight groups
+  	overrides = function(c)
+    	return {
+      	htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+      	DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+      	-- this will remove the highlight groups
+      	TSField = {},
+	}
+	end
 })
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -293,6 +293,28 @@ return require('packer').startup(function(use)
 	use 'nvim-treesitter/nvim-treesitter'
 	-- vimspector for debuging
 	use 'puremourning/vimspector'
+	use({
+  	'projekt0n/github-nvim-theme',
+  	config = function()
+    require('github-theme').setup({
+      	theme_style = "dark",
+  		function_style = "italic",
+  		sidebars = {"qf", "vista_kind", "terminal", "packer"},
 
+  		-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  		colors = {hint = "orange", error = "#ff0000"},
+
+  		-- Overwrite the highlight groups
+  		overrides = function(c)
+    	return {
+      		htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
+      		DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
+      		-- this will remove the highlight groups
+      		TSField = {},
+    	}
+  			end
+   		})
+  		end
+	})
 end)
 
