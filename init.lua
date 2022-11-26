6f9e72ed3ca3e8eff171a3a0b4cd04ebaf718370
John Doe <johndoe@example.com>
2022-11-26 10:59:29 -0600

    Added autocomplete but need to correct lsp server stuff for every language

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ require("feline").setup()
 require("plugins/autosave")
 require("plugins/diffview")
 require("plugins/lspconfig")
+
 -- tabby config
 local theme = {
   fill = 'TabLineFill',
@@ -106,32 +107,6 @@ vim.cmd([[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
-
--- Completition plugins
-local cmp = require'cmp'
-cmp.setup({
-  -- Enable LSP snippets
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    })
-  },
-}) 
 
 
 -- Lua github-theme
